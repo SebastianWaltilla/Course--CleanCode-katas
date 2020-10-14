ba3625bd726e7f450bf582b7321cc80218217f7b
sebas <sebastianwaltilla@hotmail.com>
2020-10-14 11:24:34 +0200

    first commit

---
+++ src/test/java/BioTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BioTest {
+    @Test
+    public void testDna() throws Exception {
+        Bio b = new Bio();
+        assertEquals("UUUU", b.dnaToRna("TTTT"));
+    }
+
+    @Test
+    public void testDna2() throws Exception {
+        Bio b = new Bio();
+        assertEquals("GCAU", b.dnaToRna("GCAT"));
+    }
+}
