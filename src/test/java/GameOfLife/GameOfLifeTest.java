39cb9a992840ad3ae71fcd673da4de7fea185f4c
sebas <sebastianwaltilla@hotmail.com>
2020-10-21 15:40:13 +0200

    start

--- src/test/java/GameOfLife/GameOfLifeTest.java
+++ src/test/java/GameOfLife/GameOfLifeTest.java
@@ -31,7 +31,16 @@ class GameOfLifeTest {
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0}
         };
-        testGrid = new Grid(gridAsArray);
+
+        int[][] gridAsArray3 = new int[][]{
+                {1, 0, 0},
+                {1, 1, 0},
+                {1, 0, 1}
+        };
+
+
+
+        testGrid = new Grid(gridAsArray3);
         gol = new GameOfLife(testGrid);
     }
 
@@ -43,17 +52,34 @@ class GameOfLifeTest {
     @Test
     void checkIfCellIsAlive(){
 
-        //Cell cell = new Cell(0, 0);
-        //assertTrue(gol.checkAliveCell(cell));
-
-
-
-
-
     }
 
 
+   /*
 
+    @DisplayName("1. Any live cell with fewer than two live neighbors" +
+            " dies, as if caused by underpopulation.")
+
+    }
+    @Test
+    @DisplayName(" 2. Any live cell with more than three live neighbors dies, as if by" +
+            "    overcrowding.")
+    void method2(){
+
+    }
+    @Test
+    @DisplayName(" 3. Any live cell with two or three live neighbors lives on to the next" +
+            "    generation.")
+    void method3(){
+
+    }
+    @Test
+    @DisplayName("4. Any dead cell with exactly three live neighbors becomes a live cell.")
+    void method4(){
+
+    }
+
+     */
 
 
 
